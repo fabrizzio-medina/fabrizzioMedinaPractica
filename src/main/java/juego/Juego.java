d57f45fe4c19d7d9e7cf93dcaeea04b223a9c428
Fabrizzio <fabrigamer0303@gmail.com>
2026-05-18 06:31:23 +0200

    he añadido una tabla de clasificacion de los 5 primeros que se guardan en un archivo txt

--- src/main/java/juego/Juego.java
+++ src/main/java/juego/Juego.java
@@ -1,33 +1,38 @@
 package juego;
 
 import dao.JugadorDAO;
+import dao.ScoreDAO;
 import mapa.Mapa;
 import modelo.Jugador;
+import score.Score;
 
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Juego {
     private Jugador jugador = new Jugador();// Objeto jugador que representa al jugador durante toda la partida
     private Mapa mapa;// Objeto mapa que contiene todas las zonas del juego
 
-
+    private Score tablaClasificacion = new Score();
 
     private JugadorDAO jugadorDAO;
-
+    private ScoreDAO scoreDAO;
 
     /*
      * Constructor de la clase Juego
      * Recibe el Scanner para poder usarlo en las zonas
      */
-    public Juego(Scanner sc, JugadorDAO jugadorDAO) {
+    public Juego(Scanner sc, JugadorDAO jugadorDAO, ScoreDAO scoreDAO) {
         this.mapa = new Mapa(sc);
         this.jugadorDAO = jugadorDAO;
-
+        this.scoreDAO = scoreDAO;
     }
 
     public void iniciar() {
 
+        cargarTablaClasificacion();
 
         do {
             jugador = escogerModoJuego();
@@ -39,7 +44,7 @@ public class Juego {
          * Bucle principal , se continua mientras el jugador tenga vida y el juego no
          * este completado
          */
-        do{
+        do {
             /*
              * Muestra la rabia restante , muestra las zonas del mapa y permite al jugador
              * seleccionar la zona
@@ -50,32 +55,33 @@ public class Juego {
             System.out.println(jugador.mostrarEnConsola());
             System.out.println();
 
-            if(jugador.estaVivo()){
+            if (jugador.estaVivo()) {
                 mapa.mostrarZonas();
                 mapa.seleccionarZona(jugador);
                 System.out.println();
             }
-        }while (jugador.estaVivo() && !mapa.juegoCompletado() && mapa.continuarJuego());
+        } while (jugador.estaVivo() && !mapa.juegoCompletado() && mapa.continuarJuego());
 
         /* Si el jugador se queda sin rabia pierde y si completa todas las zonas gana */
         if (!jugador.estaVivo()) {
             System.out.println("Has perdido toda tu rabia...");
-        }else {
-            if(mapa.juegoCompletado()){
+        } else {
+            if (mapa.juegoCompletado()) {
                 System.out.println("¡Has derrotado a Zeus!");
-            }else{
+            } else {
                 System.out.println("Adios ...");
             }
         }
 
         jugadorDAO.actualizar(jugador);
-
+        tablaClasificacion.guardar(jugador);
+        scoreDAO.guardarArchivo(tablaClasificacion.getMapaScore());
     }
 
-    public Jugador escogerModoJuego(){
+    public Jugador escogerModoJuego() {
         int op = mapa.imprimirOpcionesJuego();
 
-        switch (op){
+        switch (op) {
             case 1: // ES JUGADOR NUEVO
                 jugador = mapa.crearJugador();
                 jugador = jugadorDAO.guardar(jugador);
@@ -83,13 +89,16 @@ public class Juego {
             case 2: // RECUPERAR POR ID
                 int id = mapa.crearIdJugador();
                 jugador = jugadorDAO.buscar(id);
-                if(jugador != null){
+                if (jugador != null) {
                     mapa.cargarZonas(jugador);
-                }else{
+                } else {
                     System.out.println("ERROR - el jugador con ID : " + id + ", NO EXISTE");
                 }
                 break;
-            case 3:
+            case 3: // Mostrar tabla clasificaciones
+                tablaClasificacion.mostrar();
+                jugador = null;
+
                 break;
             case 4: //Salir
                 System.out.println("Gracias por jugar Nuestro juego - Dev. Fabrizzio");
@@ -102,5 +111,9 @@ public class Juego {
         return jugador;
     }
 
+    public void cargarTablaClasificacion() {
+        Map<Integer, List<String>> mapaRecuperado = scoreDAO.leerDataArchivo();
+        tablaClasificacion = new Score(mapaRecuperado);
+    }
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
 import dao.JugadorDAO;
+import dao.ScoreDAO;
 import dao.impl.JugadorDAOMysqlImpl;
+import dao.impl.ScoreDAOArchivoImpl;
 import juego.Juego;
 
 
@@ -11,15 +13,17 @@ public class Main {
     public static void main(String[] args) {
 
 
+        // Se crea un objeto Scanner para leer
+        // la entrada del usuario desde la consola
         Scanner sc = new Scanner(System.in);
         JugadorDAO jugadorDAO = new JugadorDAOMysqlImpl();
-
+        ScoreDAO scoreDAO = new ScoreDAOArchivoImpl();
 
         // Se crea el objeto Juego y se le pasa el Scanner
         // para que todas las clases del juego puedan leer datos del usuario
 
 
-        Juego juego = new Juego(sc, jugadorDAO);
+        Juego juego = new Juego(sc, jugadorDAO, scoreDAO);
         juego.iniciar();
 
         sc.close();
